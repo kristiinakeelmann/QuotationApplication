f21d61a3e12e40e8236f68a7e56dc01add28e859
kristiinakeelmann <kristiinakeelmann@gmail.com>
2017-01-28 13:38:56 +0200

    New project

---
+++ src/Database.java
@@ -0,0 +1,125 @@
+import java.sql.*;
+import java.util.ArrayList;
+
+//Kristeri koodi põhjal
+class Database {
+
+    Connection conn = null;
+
+    private void initDatabase() {
+        createConnection();
+        createTable();
+    }
+
+    // Et andmebaasi kasutada peame sellega esiteks ühenduse looma
+    public void createConnection() {
+        try {
+            Class.forName("org.sqlite.JDBC");                          // Lae draiver sqlite.jar failist
+            this.conn = DriverManager.getConnection("jdbc:sqlite:test.db"); // loo ühendus andmebaasi failiga
+        } catch (Exception e) {                                      // püüa kinni võimalikud errorid
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+        System.out.println("Opened initDatabase successfully");            // lihtsalt meie enda jaoks teade
+    }
+
+    // Et andmebaasist kasu oleks, loome uue tabeli. See on nagu uus 'sheet' excelis.
+    public void createTable() {
+        // Käsk ise on CREATE TABLE ja sulgude vahel on kõik tulbad, mida tahan, et tabel hoiaks.
+        String sql = "CREATE TABLE TSITAAT (ID INT AUTO_INCREMENT, TSITAAT TEXT, AASTA INT, MARKSONA TEXT)";
+        updateTable(sql);
+    }
+
+    public void enterQuotation(String tsitaat, String aasta, String marksona) {
+        // Andmete sisestamiseks on käsk INSERT. Esimestes sulgudes tulbad KUHU salvestada,teistes sulgudes VALUES() MIDA salvestada.
+        String sql = "INSERT INTO TSITAAT (TSITAAT, AASTA, MARKSONA) VALUES ('" + tsitaat + "','" + aasta + "','" + marksona + "')";
+        updateTable(sql);
+    }
+
+    public void selectTable() {
+        try {
+            Statement stat = this.conn.createStatement();
+            stat.execute("SELECT * FROM TSITAAT");
+
+            ResultSet rs = stat.executeQuery("SELECT * FROM TSITAAT");
+            // Kui stat.executeQuery() toob tagasi tühja tulemuse, siis rs'i kasutada ei saa.
+
+            // tsükkel
+            Integer mitmes = 0;
+            while (rs.next()) {
+                mitmes = mitmes + 1;// mitmes++;
+                System.out.println("Andmed: " + mitmes.toString() + rs.getString("ID") + ", " + rs.getString("TSITAAT") + ", " + rs.getString("AASTA") + ", " + rs.getString("MARKSONA"));
+            }
+
+            rs.close();
+            stat.close(); // Statement tuleb samuti kinni panna nagu ka Connection.
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public ArrayList<String> selectQuoatitonByYearAndKeyword(String year, String keyWord) {
+        try {
+            Statement stat = this.conn.createStatement();
+            ResultSet rs = stat.executeQuery("SELECT tsitaat FROM TSITAAT WHERE AASTA = '" + year + "' AND marksona = '" + keyWord + "'");
+            ArrayList<String> quotesFromDatabase = new ArrayList<String>();
+            while (rs.next()) {
+                quotesFromDatabase.add(rs.getString("tsitaat"));
+                break;
+                // ei oota ära kõiki vastuseid, vaid võta mulle esimene leitud väärtus
+            }
+            rs.close();
+            stat.close(); // Statement tuleb samuti kinni panna nagu ka Connection.
+            return quotesFromDatabase;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return new ArrayList<String>();
+        }
+    }
+
+    public ArrayList<String> selectColumn(String value) {
+        try {
+            Statement stat = this.conn.createStatement();
+            ResultSet rs = stat.executeQuery("SELECT " + value + " FROM TSITAAT");
+            ArrayList<String> valueFromDatabase = new ArrayList<String>();
+            while (rs.next()) {
+                valueFromDatabase.add(rs.getString(value));
+            }
+            rs.close();
+            stat.close(); // Statement tuleb samuti kinni panna nagu ka Connection.
+            return valueFromDatabase;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return new ArrayList<String>();
+        }
+    }
+
+
+    // Andmebaasi muudatused ei tagasta väärtusi (erinevalt päringutest) ja on lihtne eraldi meetodi tuua.
+    private void updateTable(String sql) {
+        try {
+            // Statement objekt on vajalik, et SQL_Login käsku käivitada
+            if (this.conn == null) {
+                return;
+            }
+            Statement stat = this.conn.createStatement();
+            stat.executeUpdate(sql);
+            stat.close(); // Statement tuleb samuti kinni panna nagu ka Connection.
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Kui programmis avad ainult ühendusi ja ühtegi ei sulge siis see võib masina kokku jooksutada.
+    public void sulgeYhendus() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Ühendus suletud");
+    }
+
+}
---
+++ src/Tsitaadid.java
@@ -0,0 +1,173 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.*;
+
+
+public class Tsitaadid extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        final Database dbcon = new Database();
+        dbcon.createConnection();
+        dbcon.selectTable();
+
+        Stage stage = new Stage();
+        StackPane stackPane = new StackPane();
+        BorderPane borderPane = new BorderPane();
+        stackPane.getChildren().addAll(borderPane);
+        Scene scene = new Scene(stackPane, 600, 600);
+        stage.setScene(scene);
+        stage.show();
+
+
+        ComboBox<String> yearCombobox = new ComboBox<String>();
+        ArrayList<String> yearComboboxValue = new ArrayList<String>();
+        try{
+            yearComboboxValue = dbcon.selectColumn("AASTA");
+        } catch (Exception e){
+            e.printStackTrace();
+            yearComboboxValue = new ArrayList<String>();
+        }
+        yearCombobox.getItems().addAll(yearComboboxValue);
+
+
+        ComboBox<String> keywordCombobox = new ComboBox<String>();
+        ArrayList<String> keywordComboboxValue = new ArrayList<String>();
+        try {
+            keywordComboboxValue = dbcon.selectColumn("MARKSONA");
+        } catch (Exception e ) {
+            e.printStackTrace();
+            keywordComboboxValue = new ArrayList<String>();
+        }
+
+        keywordCombobox.getItems().addAll(keywordComboboxValue);
+
+
+        Button findQuotationButton = new Button("Leia tsitaat valiku põhjal!");
+
+        Button sendEmailButton = new Button("Saada!");
+
+
+        Text quotationText = new Text();
+
+        TextField emailTextField = new TextField();
+       // JLabel myLabel1 = new JLabel("Saada tsitaat oma e-mailile!");
+        emailTextField.setPromptText("Sisesta oma e-mail");
+
+        Text emailSendingMessageTextField = new Text();
+
+
+        TextField quotationTextField = new TextField();
+         // JLabel myLabel2 = new JLabel("Sisesta uus tsitaat");
+        quotationTextField.setPromptText("Sisesta tsitaat");
+
+        TextField yearTextField = new TextField();
+        yearTextField.setPromptText("Sisesta ilmumisaasta");
+
+        TextField keywordTextField = new TextField();
+        keywordTextField.setPromptText("Sisesta märksõna");
+
+        Button saveQuotationButton = new Button("Salvesta!");
+
+        VBox vBox = new VBox(yearCombobox, keywordCombobox, findQuotationButton, quotationText, emailTextField, sendEmailButton, emailSendingMessageTextField, quotationTextField, yearTextField, keywordTextField, saveQuotationButton);
+        vBox.setSpacing(20);
+        borderPane.setCenter(vBox);
+
+
+        findQuotationButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+                final Database dbcon = new Database();
+                dbcon.createConnection();
+                dbcon.selectTable();
+
+                //muutuja, nimi, selle hilisem kasutamine
+                ArrayList<String> result = dbcon.selectQuoatitonByYearAndKeyword(yearCombobox.getValue(), keywordCombobox.getValue());
+                if (result.size() > 0) {
+                    quotationText.setText(result.get(0));
+                } else {
+                    quotationText.setText("Ei leidu tsitaati");
+                }
+
+                }
+
+        });
+
+        sendEmailButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if (emailTextField.getText().toString().equalsIgnoreCase("")) {
+                    emailSendingMessageTextField.setText("Sisesta palun e-mail!");
+                    return;
+                }
+
+                //kasutatud kasutaja anirudh koodi (loodud 28/10/14) põhjal
+
+                final String username = "tsitaadirakendus@gmail.com";
+                final String password = "12345tsitaadid";
+
+                Properties props = new Properties();
+                props.put("mail.smtp.host", "smtp.gmail.com");
+                props.put("mail.smtp.socketFactory.port", "465");
+                props.put("mail.smtp.socketFactory.class",
+                        "javax.net.ssl.SSLSocketFactory");
+                props.put("mail.smtp.auth", "true");
+                props.put("mail.smtp.port", "465");
+                try {
+
+                    Session session = Session.getDefaultInstance(props,
+                            new javax.mail.Authenticator() {
+                                protected PasswordAuthentication getPasswordAuthentication() {
+                                    return new PasswordAuthentication(username, password);
+                                }
+                            });
+                    Message message = new MimeMessage(session);
+                    message.setFrom(new InternetAddress("tsitaadirakendus@gmail.com"));
+                    message.setRecipients(Message.RecipientType.TO,
+                            InternetAddress.parse(emailTextField.getText().toString()));
+                    message.setSubject("Kiri tsitaadirakenduselt");
+                    message.setText("Sinu valitud tsitaat:" +
+                            quotationTextField.getText().toString());
+                    Transport.send(message);
+
+                    System.out.println("Mail sent succesfully!");
+                    emailSendingMessageTextField.setText("E-mail edastatud!");
+
+                } catch (MessagingException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        });
+
+
+                saveQuotationButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+
+                    if (quotationTextField.getText().toString().equalsIgnoreCase("")) {
+                        return;
+                    }
+
+                    dbcon.enterQuotation(quotationTextField.getText().toString(), yearTextField.getText().toString(), keywordTextField.getText().toString());
+
+                }
+        });
+
+    }
+}
